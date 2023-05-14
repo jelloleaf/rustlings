9134ba06a98c328f1c5cdff809b73618ddc55319
JelloLeaf <JelloleafGit@gmail.com>
2023-05-14 10:26:56 -0400

    iterators4

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,9 +1,12 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
+    (1..num+1).product()
+
+    }
+
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
@@ -13,7 +16,6 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
-}
 
 #[cfg(test)]
 mod tests {
