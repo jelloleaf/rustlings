da9a8ffb48212c646d549e3eddfaed05726e667e
JelloLeaf <JelloleafGit@gmail.com>
2023-05-17 07:36:23 -0400

    iter5

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -8,7 +8,6 @@
 // need to be modified.
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -51,7 +50,7 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!()
+    collection.iter().map(|x| count_iterator(x, value)).sum()
     
 }
 
